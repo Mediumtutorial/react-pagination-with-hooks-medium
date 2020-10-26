8292d5e3b60ddd58f55bfce9b5e9df9d710fcf6d
Manish Mandal <manish.mandal21@gmail.com>
2020-10-26 21:49:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+import ReactPaginate from 'react-paginate';
+import './App.css'
 
 function App() {
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0)
+
+
+  const getData = async() => {
+      const res = await axios.get(`https://jsonplaceholder.typicode.com/photos`)
+      const data = res.data;
+                const slice = data.slice(offset, offset + perPage)
+                const postData = slice.map(pd => <div key={pd.id}>
+                    <p>{pd.title}</p>
+                    <img src={pd.thumbnailUrl} alt=""/>
+                </div>)
+                setData(postData)
+                setPageCount(Math.ceil(data.length / perPage))
+  }
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1)
+};
+
+ useEffect(() => {
+   getData()
+ }, [offset])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
+       <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={pageCount}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
     </div>
   );
 }
